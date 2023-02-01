95abcd52d5d777975ebacb2490d66694ec36990d
GitOffMyLawn21 <bsauvage@evolytics.com>
2023-02-01 12:16:08 -0600

    Adding component hiding logic on lift ticket booking page

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let snowInterval = setInterval(function(){
-    if($(".c126__total1--v1.header_weather__snowfall_measurement").length > 0){
+    if($(".c126__total1--v1.header_weather__snowfall_measurement").length){
         toggleWeatherWidget();
         clearInterval(snowInterval);
 ;    }
@@ -17,26 +17,37 @@ function toggleWeatherWidget(){
     if(typeof(snowMeasurement) === 'number'){
 
         // Hide the 'Todays Forecast' part of the widget
-        $("#c126_WeatherHeader_1 .header_weather__forecast").hide()
+        $("#c126_WeatherHeader_1 .header_weather__forecast, .header_weather__lift_status").hide()
 
-        // Begin keeping track of the time that the page has effectively loaded. Globally scoped, of course.
-        var start = Date.now();
+        if(snowMeasurement >= 2){
+            // Toggle the appropriate components
+            $("#webcam_full_width_2").hide();
+            $("#webcam_full_width_1").show();
+            $(".weather_detail").show();
+            
+            // Begin keeping track of the time that the page has effectively loaded.
+            var start = Date.now();
+            
+            // Wait half a second to show the widget by triggering a mouseover event. Send analytics tracking call as well.
+            setTimeout(function(){
+                $("#c126_WeatherHeader_1").trigger("mouseover");
+                trackAnalytics("Snow Reporting | Snow Shown | "+snowMeasurement+" inches");
 
-        // Wait half a second to show the widget by triggering a mouseover event. Send analytics tracking call as well.
-        setTimeout(function(){
-            $("#c126_WeatherHeader_1").trigger("mouseover");
-            trackAnalytics("Snow Reporting | Snow Shown | "+snowMeasurement+" inches");
-
-            // We need to add an event to the document to listen for scrolling, since it is one of our two  conditions for hiding the widget.
-            document.addEventListener("scroll", hideWeatherWidget);
-        }, 500)
+                // We need to add an event to the document to listen for scrolling, since it is one of our two  conditions for hiding the widget.
+                document.addEventListener("scroll", hideWeatherWidget);
+            }, 500)
+        }else{
+            $("#webcam_full_width_1").hide();
+            $(".weather_detail").hide();
+            $("#webcam_full_width_2").show();
+        }
     }
     
     // Callback function that will eventually hide the widget when two conditions have been met: 1) Visitor scrolls, and 2) At least three seconds have elapsed since page load
     function hideWeatherWidget(){
         // Get the number of seconds that have elapsed
         let delta = Math.floor((Date.now() - start)/1000); 
-        if(delta > 3){
+        if(delta >= 3){
             // Trigger mouseout event to hide the widget, and then remove the event listener since it is no longer needed
             $("#c126_WeatherHeader_1").trigger("mouseout");
             document.removeEventListener("scroll", hideWeatherWidget);
@@ -51,4 +62,10 @@ function trackAnalytics(pev2){
         window.s.pev2 = pev2;
         window.s.tl();
     }
+}
+
+// Function to change snow displayed in widget  -- for testing purposes only
+function changeSnowAmount(num){
+    $(".c126__label1--v1:contains('24 HourSnowfall'):first").parent().children().first().text(`${num}in`)
+    toggleWeatherWidget();
 }
